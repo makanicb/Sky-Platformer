1600f875787d10755de04f931855092acc0a1e50
makanicb <makanicb25@gmail.com>
2024-04-23 09:54:14 -0700

    Clamped camera rotation

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,24 +8,32 @@ public class CameraController : MonoBehaviour
     private Vector2 delta;
     private Transform _TF;
     private Transform _LF;
+    //Camera movement and placement
     public float speed;
     public Transform Camera;
     public float maxCameraDist;
+    public float maxAngle, minAngle;
+    private float currentAngle;
     // Start is called before the first frame update
     void Start()
     {
         delta = new Vector2(0f, 0f);
         _TF = gameObject.GetComponent<Transform>();
         _LF = _TF.GetChild(0).GetComponent<Transform>();
+        currentAngle = _LF.localEulerAngles.x;
     }
 
     // Update is called once per frame
     void Update()
     {
         _TF.Rotate(0f, delta.x * speed * Time.deltaTime, 0f, Space.World);
-        _LF.Rotate(delta.y * speed * Time.deltaTime, 0f, 0f, Space.Self);
+        float deltaAngle = delta.y * speed * Time.deltaTime;
+        float trueNewAngle = Mathf.Clamp(currentAngle + deltaAngle, minAngle, maxAngle);
+        float trueDeltaAngle = trueNewAngle - currentAngle;
+        _LF.Rotate(trueDeltaAngle, 0f, 0f, Space.Self);
+        currentAngle += trueDeltaAngle;
+        Debug.Log(currentAngle);
         Debug.DrawRay(_TF.position, _LF.forward * maxCameraDist);
-        
     }
 
     private void FixedUpdate()
@@ -41,8 +49,9 @@ public class CameraController : MonoBehaviour
         {
             cameraPosition = _LF.position + cameraDir * maxCameraDist;
         }
-        Camera.position = cameraPosition;
-        Camera.LookAt(_LF);
+
+        Camera.SetPositionAndRotation(cameraPosition, _LF.rotation);
+        //Camera.LookAt(_LF);
     }
 
     void OnLook(InputValue lookValue)
