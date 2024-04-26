ef3cbb0a6ff21bcd454ad44941a7ccec599f1f8d
makanicb <makanicb25@gmail.com>
2024-04-26 16:31:13 -0700

    Added a time limiton bullet time and a recharge function.

--- Assets/Scripts/BulletTimeController.cs
+++ Assets/Scripts/BulletTimeController.cs
@@ -9,11 +9,23 @@ public class BulletTimeController : MonoBehaviour
     private float timeScale;
     public float slowScale;
     private float normScale;
+
     //Speed to switch between modes
     public float shiftSpeed;
     private float shiftVel;
+
     //the default fixedDeltaTime
     private float fixedDeltaTime;
+
+    //Ability limiter
+    public float duration;
+    public float chargeRate;
+    private float rDuration;
+    private float rate;
+
+    //ability toggle
+    private bool slow;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +33,9 @@ public class BulletTimeController : MonoBehaviour
         normScale = this.timeScale;
         shiftVel = shiftSpeed;
         this.fixedDeltaTime = Time.fixedDeltaTime;
+        rDuration = duration;
+        rate = chargeRate;
+        slow = false;
     }
 
     // Update is called once per frame
@@ -29,11 +44,27 @@ public class BulletTimeController : MonoBehaviour
         this.timeScale = Mathf.Clamp(this.timeScale + shiftVel * Time.unscaledDeltaTime, slowScale, normScale);
         Time.timeScale = this.timeScale;
         Time.fixedDeltaTime = this.fixedDeltaTime * this.timeScale;
+        rDuration = Mathf.Clamp(rDuration + rate * Time.deltaTime, 0f, duration);
+        //Debug.Log(rDuration);
+        if(rDuration == 0)
+        {
+            slow = false;
+            rate = chargeRate;
+            shiftVel = shiftSpeed;
+        }
     }
 
     void OnSlowTime()
     {
-        Debug.Log("Slow Time");
         shiftVel *= -1;
+        slow = !slow;
+        if(slow)
+        {
+            rate = -1f;
+        }
+        else
+        {
+            rate = chargeRate;
+        }
     }
 }
