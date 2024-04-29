ed6d1d21ccfffc46176ba84a67791738ca8480e3
makanicb <makanicb25@gmail.com>
2024-04-28 21:25:47 -0700

    Made explosion force on objects porpotional to angle of approach.

--- Assets/Scripts/BallOfForceController.cs
+++ Assets/Scripts/BallOfForceController.cs
@@ -7,6 +7,7 @@ public class BallOfForceController : MonoBehaviour
     public float explosionForce;
     public float upwardMod;
     private Transform _TF;
+    public Rigidbody _PRB;
     private float explosionRadius;
     // Start is called before the first frame update
     void Start()
@@ -26,16 +27,25 @@ public class BallOfForceController : MonoBehaviour
         //Debug.Log("EXPLOSION");
         Rigidbody rb = other.attachedRigidbody;
         Transform tf = other.transform;
-        if(rb != null && !_TF.IsChildOf(tf))
-            rb.AddExplosionForce(explosionForce, _TF.position, Mathf.Infinity, upwardMod, ForceMode.Impulse);
+        if (rb != null && !_TF.IsChildOf(tf))
+        {
+            Vector3 offset = tf.position - _TF.position;
+            Vector3 dir = offset.normalized;
+            Vector3 velDir = _PRB.velocity.normalized;
+            //Debug.Log("dir " + dir);
+            //Debug.Log("vel " + velDir);
+            float expForce = Mathf.Max(0f, explosionForce * Vector3.Dot(velDir, dir));
+            //Debug.Log(expForce);
+            rb.AddExplosionForce(expForce, _TF.position, Mathf.Infinity, upwardMod, ForceMode.Impulse);
+        }
         //Debug.Log(other.gameObject.name);
         if(tf.gameObject.CompareTag("genericEnemy"))
         {
             EnemyController enemyController = tf.gameObject.GetComponent<EnemyController>();
-            Debug.Log("Hurt enemy through dash");
-            Debug.Log(enemyController);
+            //Debug.Log(enemyController);
             if(enemyController != null)
             {
+                //Debug.Log("Hurt enemy through dash");
                 enemyController.health--;
                 Debug.Log("Remaining health " + enemyController.health);
                 if(enemyController.health == 0)
