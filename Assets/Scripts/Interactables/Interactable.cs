1a0c1b3885660f3eb45bf92f3cad3d7ff0e3d531
anmaiiii <anmai8376@gmail.com>
2024-05-10 19:31:32 -0700

    minor tweaks in scripts for inventory

--- Assets/Scripts/Interactables/Interactable.cs
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -5,12 +5,7 @@ using UnityEngine.AI;
 public class Interactable : MonoBehaviour {
 
 	public float radius = 3f;
-	public Transform interactionTransform;
 
-	bool isFocus = false;	// Is this interactable currently being focused?
-	// Transform player;		// Reference to the player transform
-
-	bool hasInteracted = false;	// Have we already interacted with the object?
 
 	void Update ()
 	{
@@ -42,21 +37,6 @@ public class Interactable : MonoBehaviour {
         }
     }
 
-	// Called when the object starts being focused
-	public void OnFocused (Transform playerTransform)
-	{
-		isFocus = true;
-		// hasInteracted = false;
-		// player = playerTransform;
-    }
-
-	// Called when the object is no longer focused
-	public void OnDefocused ()
-	{
-		isFocus = false;
-		// hasInteracted = false;
-		// player = null;
-	}
 
 	// This method is meant to be overwritten
 	public virtual void Interact ()
@@ -64,15 +44,6 @@ public class Interactable : MonoBehaviour {
 		Debug.Log("Item picked up!");
 	}
 
-	void OnDrawGizmosSelected ()
-	{
-		if (interactionTransform == null) {
-			interactionTransform = transform;
-		}
-		
-		Gizmos.color = Color.yellow;
-		Gizmos.DrawWireSphere(interactionTransform.position, radius);
-	}
 
 
 }
