67883661e12bf03b9ac8f22105d4230514e6eef3
makanicb <makanicb25@gmail.com>
2024-05-20 12:45:55 -0700

    Created new Damageable script to be inherited by dameagable objects.

---
+++ Assets/Scripts/Dameagable.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dameagable : MonoBehaviour
+{
+    public int health;
+    public int maxHealth;
+
+    public void damage(int dmg)
+    {
+        health -= dmg;
+        health = Mathf.Min(health, maxHealth);
+        if (health == 0)
+            onDeath();
+    }
+
+    protected virtual void onDeath()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
