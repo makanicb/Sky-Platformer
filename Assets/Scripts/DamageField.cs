dfe85ceebcd36ab17cd2909e943773f27c07bec8
makanicb <makanicb25@gmail.com>
2024-05-20 23:12:02 -0700

    Created DamageField for managing colliders that deal damage.

---
+++ Assets/Scripts/DamageField.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageField : MonoBehaviour
+{
+    [SerializeField] private int damage;
+    [SerializeField] private List<string> targets;
+
+    
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        foreach(string t in targets)
+        {
+            if(other.gameObject.CompareTag(t))
+            {
+                Damageable dam = other.GetComponent<Damageable>();
+                if(dam is not null)
+                {
+                    dam.damage(damage);
+                }
+            }
+        }
+    }
+}
