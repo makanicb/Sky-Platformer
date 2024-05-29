0812b1cb2bd7a0f8470d4af7dc83382bf325cc63
makanicb <makanicb25@gmail.com>
2024-05-29 01:33:55 -0700

    Created loader for scene two.

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] private List<string> scenesToLoad;
+    [SerializeField] private List<string> scenesToUnload;
+
+    public void LoadScenes()
+    {
+        foreach(string scene in scenesToLoad)
+        {
+            if(!SceneManager.GetSceneByName(scene).isLoaded)
+            {
+                Debug.Log("Load Scene" + scene);
+                SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
+            }
+        }
+        foreach(string scene in scenesToUnload)
+        {
+            if(SceneManager.GetSceneByName(scene).isLoaded)
+            {
+                Debug.Log("Load Scene" + scene);
+                SceneManager.UnloadSceneAsync(scene);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+            LoadScenes();
+    }
+}
