77e2e0bf036d3ae7d246424cd4bc5dd63f33eae5
anmaiiii <anmai8376@gmail.com>
2024-06-05 00:01:37 -0700

    added ambient sounds, footsteps, pickup, and grapple sounds

---
+++ Assets/Scripts/FootstepScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepScript : MonoBehaviour
+{
+    public GameObject footstep;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        footstep.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("w") || Input.GetKey("s") || Input.GetKey("a") || Input.GetKey("d"))
+        {
+            footsteps();
+        }
+
+        if(Input.GetKeyUp("w") || Input.GetKeyUp("s") || Input.GetKeyUp("a") || Input.GetKeyUp("d"))
+        {
+            StopFootsteps();
+        }
+
+    }
+
+    void footsteps()
+    {
+        footstep.SetActive(true);
+    }
+
+    void StopFootsteps()
+    {
+        footstep.SetActive(false);
+    }
+}
